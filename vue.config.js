2a42ce5de5f6bc833bc1c89697ae9842ae8778f5
sangshaofeng <sangshaofeng@kungeek.com>
2019-07-29 11:53:11 +0800

    修改生产环境配置

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/tool-box/' : '/',
   outputDir: 'dist',
   assetsDir: 'static',
   devServer: {},
